358ff70d78fc3d0c367e9ebfd86a7b3b3e9439b7
vhorozheiev <vhorozheiev@gmail.com>
2022-01-05 17:01:58 +0200

    added fill array method

--- src/Main.java
+++ src/Main.java
@@ -1,42 +1,31 @@
 import java.util.Arrays;
+import java.util.List;
 import java.util.Random;
 
 public class Main {
-    public static void main(String[] args) {
-        Figure circle = new Circle("green", 5);
-        circle.draw();
-        circle.getArea();
-        circle.getColor();
-        System.out.println("The radius of the circle is " + ((Circle) circle).getRadius());
-        System.out.println("/////////////////////////////////////////////////////////");
-        Figure square = new Square("blue", 10);
-        square.getArea();
-        square.getColor();
-        square.draw();
-        System.out.println("/////////////////////////////////////////////////////////");
-        Figure triangle = new Triangle("white", 5, 4, 6);
-        triangle.getColor();
-        triangle.draw();
-        triangle.getArea();
 
+    static int arraySize = 10;
 
-//        Object [] o = fill();
-//        System.out.println(Arrays.toString(o));
+    public static Object[] fillArray() {
+        Object[] array = new Object[arraySize];
+        Object obj = null;
+        Random randomizer = new Random();
+        for (int i = 0; i < 10; i++) {
+            int randomNumber = randomizer.nextInt(4);
+            switch (randomNumber) {
+                case 0 -> obj = new Circle("red", 10);
+                case 1 -> obj = new Square("black", 6);
+                case 2 -> obj = new Triangle("white", 4, 5, 6);
+                case 3 -> obj = new Trapeze("green", 4, 4, 3, 3);
+            }
+            array[i] = obj;
+        }
+        return array;
     }
-//    static int arraySize = 10;
-//    public static Object[] fill() {
-//        Object[] array = new Object[arraySize];
-//        Object obj = null;
-//        Random randomizer = new Random();
-//        for (int i = 0; i < 10; i++) {
-//            int randomNumber = randomizer.nextInt(3);
-//            switch (randomNumber) {
-//                case 0 -> obj = new Circle("red", 10);
-//                case 1 -> obj = new Square("black", 6);
-//                case 2 -> obj = new Triangle();
-//            }
-//            array[i] = obj;
-//        }
-//        return array;
-//    }
+
+    public static void main(String[] args) {
+        Object[] figures = fillArray();
+        List<Object> l = Arrays.asList(figures);
+        System.out.println(l);
+    }
 }
--- src/Trapeze.java
+++ src/Trapeze.java
@@ -1,2 +1,32 @@
-package PACKAGE_NAME;public class Trapeze {
+public class Trapeze extends Figure {
+    private String color;
+    private int firstSideLength;
+    private int secondSideLength;
+    private int thirdSideLength;
+    private int fourthSideLength;
+
+    public Trapeze(String color, int firstSideLength, int secondSideLength, int thirdSideLength, int fourthSideLength) {
+        this.color = color;
+        this.firstSideLength = firstSideLength;
+        this.secondSideLength = secondSideLength;
+        this.thirdSideLength = thirdSideLength;
+        this.fourthSideLength = fourthSideLength;
+    }
+
+    @Override
+    public void draw() {
+        System.out.println("Trapeze has been drawn");
+    }
+
+    @Override
+    public double getArea() {
+        double area = 1;
+        return area;
+    }
+
+    @Override
+    public String getColor() {
+        System.out.println("The color of triangle is " + this.color);
+        return color;
+    }
 }
